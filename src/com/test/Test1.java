6b44cce6f1f3eb98bdd6376e51df81adf95b15f3
str363636 <stailn@rewinhealth.com>
2025-07-30 15:46:09 +0530

    changes in string value

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 
 public class Test1 {
 	public static void main(String[] args) {
-		String s="stalin";
+		String s="stalin raja";
 		for (int i = 0; i <=s.length() ; i++) {
 			System.out.println("*");
 			
